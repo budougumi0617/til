66a2f4e5989f6271455313835fb41f05810ec955
budougumi0617 <budougumi0617@gmail.com>
2019-02-20 22:58:37 +0900

    :+1: Add HandlerFunc test

--- go/http/httptest/simple_test.go
+++ go/http/httptest/simple_test.go
@@ -31,3 +31,20 @@ func TestSimple_ByMux(t *testing.T) {
 		t.Errorf("want %s, but got = %s\n", want, got.Body.String())
 	}
 }
+
+func TestSimple_ByHandlerFunc(t *testing.T) {
+	path := "/simple"
+	want := "dummy body"
+	req := httptest.NewRequest("GET", path, bytes.NewBufferString(want))
+	req.Header.Set("Test", want)
+
+	got := httptest.NewRecorder()
+
+	h := http.HandlerFunc(simple)
+
+	h.ServeHTTP(got, req)
+
+	if got.Body.String() != want {
+		t.Errorf("want %s, but got = %s\n", want, got.Body.String())
+	}
+}
