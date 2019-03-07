6e21cbfa7eb34c3abab45843db55e202a53b8566
budougumi0617 <budougumi0617@gmail.com>
2019-03-07 11:00:28 +0900

    :+1: Add test pattern

--- go/gomock/client_test.go
+++ go/gomock/client_test.go
@@ -8,7 +8,86 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
+func TestReturn(t *testing.T) {
+	in := "any value"
+	want := "hoge"
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mc := mock.NewMockClient(ctrl)
+
+	mc.EXPECT().Do(in).Return("hoge", nil)
+
+	if got, _ := mc.Do(in); got != want {
+		t.Errorf("want %#v, but got %#v\n", want, got)
+	}
+
+}
+
 func TestDoAndReturn(t *testing.T) {
+	in := "value"
+	want := "value modified in mock"
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mc := mock.NewMockClient(ctrl)
+
+	mc.EXPECT().Do(in).DoAndReturn(
+		func(in string) (string, error) {
+			return fmt.Sprint(in, " modified in mock"), nil
+		})
+	if got, _ := mc.Do(in); got != want {
+		t.Errorf("want %#v, but got %#v\n", want, got)
+	}
+
+}
+
+func TestDoAndReturn2(t *testing.T) {
+
+	in := "any value"
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mc := mock.NewMockClient(ctrl)
+
+	mc.EXPECT().Do(in).DoAndReturn(
+		func(in string) (string, error) {
+			return "", fmt.Errorf("%s", in)
+		})
+
+	if _, err := mc.Do(in); err == nil {
+		t.Error("cannot get error")
+	}
+
+}
+
+func TestDoAndReturn3(t *testing.T) {
+
+	errin := "raise error"
+	specify := "specify"
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mc := mock.NewMockClient(ctrl)
+
+	mc.EXPECT().Do(gomock.Any()).DoAndReturn(
+		func(in string) (string, error) {
+			switch in {
+			case errin:
+				return "", fmt.Errorf("%s", in)
+			case specify:
+				return "!!!!!", nil
+			}
+			return in, nil
+		})
+	// ここから！！！
+	if _, err := mc.Do(errin); err == nil {
+		t.Error("cannot get error")
+	}
+
+}
+
+func TestBySubTest(t *testing.T) {
 	tests := []struct {
 		name      string
 		setClient func(*mock.MockClient, string)
