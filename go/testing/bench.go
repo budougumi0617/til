930056e728a27ea94b63ed2ec25667afda298eb9
budougumi0617 <budougumi0617@gmail.com>
2019-05-14 13:29:46 +0900

    :sparkles: Add distinct two pattern

---
+++ go/testing/bench.go
@@ -0,0 +1,25 @@
+package testing
+
+func removeDuplicate(args []string) []string {
+	m := make(map[string]bool, len(args)
+	for _, a := range args {
+		m[a] = true
+	}
+	as := make([]string, 0, len(m))
+	for k := range m {
+		as = append(as, k)
+	}
+	return as
+}
+
+func removeDuplicateOld(args []string) []string {
+	results := make([]string, 0, len(args))
+	encountered := map[string]bool{}
+	for i := 0; i < len(args); i++ {
+		if !encountered[args[i]] {
+			encountered[args[i]] = true
+			results = append(results, args[i])
+		}
+		return results
+	}
+}
