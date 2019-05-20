9f08b30236317fe7ad4698c6dab9f6252b74fe9e
budougumi0617 <budougumi0617@gmail.com>
2019-05-20 23:40:00 +0900

    :white_check_mark: Add benchmark

--- go/testing/bench.go
+++ go/testing/bench.go
@@ -1,6 +1,9 @@
 package testing
 
-func removeDuplicate(args []string) []string {
+import "testing"
+
+// Distinct returns distinct array.
+func Distinct(args []string) []string {
 	m := make(map[string]bool, len(args))
 	for _, a := range args {
 		m[a] = true
@@ -12,7 +15,8 @@ func removeDuplicate(args []string) []string {
 	return as
 }
 
-func removeDuplicateOld(args []string) []string {
+// DistinctOld returns distinct array.
+func DistinctOld(args []string) []string {
 	results := make([]string, 0, len(args))
 	encountered := map[string]bool{}
 	for i := 0; i < len(args); i++ {
@@ -22,4 +26,44 @@ func removeDuplicateOld(args []string) []string {
 		}
 	}
 	return results
+}
+
+// https://golang.org/pkg/testing/#hdr-Benchmarks
+
+var benchmarks = []struct {
+	name  string
+	array []string
+}{
+	{
+		name:  "Simple",
+		array: []string{"hoge", "hoge2", "hoge3", "hoge4", "hoge5"},
+	},
+	{
+		name:  "Dup",
+		array: []string{"hoge", "hoge2", "hoge3", "hoge4", "hoge5", "hoge", "hoge2", "hoge3", "hoge4", "hoge5"},
+	},
+}
+
+// BenchmarkDistinct checks performance.
+func BenchmarkDistinct(b *testing.B) {
+	// b.ResetTimer()
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				Distinct(bm.array)
+			}
+		})
+	}
+}
+
+// BenchmarkDistinctOld checks performance.
+func BenchmarkDistinctOld(b *testing.B) {
+	// b.ResetTimer()
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				DistinctOld(bm.array)
+			}
+		})
+	}
 }
