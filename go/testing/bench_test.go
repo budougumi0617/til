2abab70794bef983c4fca1e446c5bc7ad9b990d4
budougumi0617 <budougumi0617@gmail.com>
2019-05-22 09:35:31 +0900

    :recycle: Refactoring and add results

--- go/testing/bench_test.go
+++ go/testing/bench_test.go
@@ -1,14 +1,14 @@
 package testing
 
 import (
-	"fmt"
 	"strconv"
 	"testing"
 )
 
 // Distinct returns distinct array.
 func Distinct(args []string) []string {
-	m := make(map[string]bool, len(args))
+	// m := make(map[string]bool, len(args))
+	m := map[string]bool{}
 	for _, a := range args {
 		m[a] = true
 	}
@@ -34,25 +34,43 @@ func DistinctOld(args []string) []string {
 
 // https://golang.org/pkg/testing/#hdr-Benchmarks
 
+/*
+$ make bench
+go test -bench . -benchmem
+goos: darwin
+goarch: amd64
+pkg: github.com/budougumi0617/til/go/testing
+BenchmarkDistinct/small-4                 500000              2067 ns/op              16 B/op          1 allocs/op
+BenchmarkDistinct/large-4                   5000            208053 ns/op              16 B/op          1 allocs/op
+BenchmarkDistinct/fuge-4                       1        1796819644 ns/op              16 B/op          1 allocs/op
+BenchmarkDistinctOld/small-4              500000              2095 ns/op            1792 B/op          1 allocs/op
+BenchmarkDistinctOld/large-4               10000            158002 ns/op          163840 B/op          1 allocs/op
+BenchmarkDistinctOld/fuge-4                    1        2708319483 ns/op        1600004096 B/op        1 allocs/op
+PASS
+ok      github.com/budougumi0617/til/go/testing 54.878s
+*/
 var benchmarks = []struct {
 	count string
+	n     int
 }{
 	{
-		count: "10",
+		count: "small",
+		n:     10,
 	},
 	{
-		count: "100",
+		count: "large",
+		n:     100,
 	},
 	{
-		count: "10000",
+		count: "fuge",
+		n:     10000,
 	},
 }
 
-func buildArray(c string) []string {
+func buildArray(n int) []string {
 	var a []string
-	n, _ := strconv.Atoi(c)
 	for i := 0; i < n; i++ {
-		e := "hoge" + c
+		e := "hoge" + strconv.Itoa(n)
 		for j := 0; j < n; j++ {
 			a = append(a, e)
 		}
@@ -64,8 +82,7 @@ func buildArray(c string) []string {
 func BenchmarkDistinct(b *testing.B) {
 	for _, bm := range benchmarks {
 		b.Run(bm.count, func(b *testing.B) {
-			a := buildArray(bm.count)
-			fmt.Printf("len(a) = %+v\n", len(a))
+			a := buildArray(bm.n)
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				Distinct(a)
@@ -78,8 +95,7 @@ func BenchmarkDistinct(b *testing.B) {
 func BenchmarkDistinctOld(b *testing.B) {
 	for _, bm := range benchmarks {
 		b.Run(bm.count, func(b *testing.B) {
-			a := buildArray(bm.count)
-			fmt.Printf("len(a) = %+v\n", len(a))
+			a := buildArray(bm.n)
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				DistinctOld(a)
