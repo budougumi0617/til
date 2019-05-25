4fd53064422568d846ca081ce713d78ade5d448d
budougumi0617 <budougumi0617@gmail.com>
2019-05-25 10:51:57 +0900

    :speech_balloon: Add result

--- go/testing/bench_test.go
+++ go/testing/bench_test.go
@@ -40,14 +40,20 @@ go test -bench . -benchmem
 goos: darwin
 goarch: amd64
 pkg: github.com/budougumi0617/til/go/testing
-BenchmarkDistinct/small-4                 500000              2067 ns/op              16 B/op          1 allocs/op
-BenchmarkDistinct/large-4                   5000            208053 ns/op              16 B/op          1 allocs/op
-BenchmarkDistinct/fuge-4                       1        1796819644 ns/op              16 B/op          1 allocs/op
-BenchmarkDistinctOld/small-4              500000              2095 ns/op            1792 B/op          1 allocs/op
-BenchmarkDistinctOld/large-4               10000            158002 ns/op          163840 B/op          1 allocs/op
-BenchmarkDistinctOld/fuge-4                    1        2708319483 ns/op        1600004096 B/op        1 allocs/op
+BenchmarkDistinct/DenseSmall-4            500000              2432 ns/op             483 B/op          2 allocs/op
+BenchmarkDistinct/DenseLarge-4             10000            171177 ns/op            7447 B/op         10 allocs/op
+BenchmarkDistinct/DenseFuge-4                  1        1581833876 ns/op          882304 B/op        190 allocs/op
+BenchmarkDistinct/SparseSmall-4           200000             10322 ns/op            7447 B/op         10 allocs/op
+BenchmarkDistinct/SparseLarge-4             2000           1076377 ns/op          879520 B/op        172 allocs/op
+BenchmarkDistinct/SparseFuge-4                 1        55133349534 ns/op       7643668608 B/op  3903707 allocs/op
+BenchmarkDistinctOld/DenseSmall-4         500000              3125 ns/op            2115 B/op          2 allocs/op
+BenchmarkDistinctOld/DenseLarge-4           5000            298426 ns/op          169494 B/op         10 allocs/op
+BenchmarkDistinctOld/DenseFuge-4               1        2420780895 ns/op        1600718560 B/op      165 allocs/op
+BenchmarkDistinctOld/SparseSmall-4        100000             12156 ns/op            7447 B/op         10 allocs/op
+BenchmarkDistinctOld/SparseLarge-4          1000           1493100 ns/op          879445 B/op        172 allocs/op
+BenchmarkDistinctOld/SparseFuge-4              1        63013130864 ns/op       7643594368 B/op  3903243 allocs/op
 PASS
-ok      github.com/budougumi0617/til/go/testing 54.878s
+ok      github.com/budougumi0617/til/go/testing 205.438s
 */
 var benchmarks = []struct {
 	count string
@@ -109,11 +115,10 @@ func buildSparseArray(n int) []string {
 
 func bench(n int, algo func(int) []string, d func([]string) []string) func(*testing.B) {
 	return func(b *testing.B) {
-		var r []string
 		a := algo(n)
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			r = d(a)
+			d(a)
 		}
 	}
 }
