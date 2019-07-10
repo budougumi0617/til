195693d7c845b27a7f2af0290ee9c6430b5fd5f1
budougumi0617 <budougumi0617@gmail.com>
2019-07-10 19:05:34 +0900

    :sparkles: use v1, and v4

--- go/modules/branch/go.mod
+++ go/modules/branch/go.mod
@@ -2,6 +2,12 @@ module github.com/budougumi0617/til/go/modules/branch
 
 go 1.12
 
-require github.com/golang-migrate/migrate/v3 v3.5.4
+require (
+	github.com/golang-migrate/migrate v1.3.2
+	github.com/golang-migrate/migrate/v3 v3.5.4
+
+	github.com/golang-migrate/migrate/v4 v4.4.0
+	gopkg.in/mattes/migrate.v1 v1.3.2 // indirect
+)
 
 replace github.com/golang-migrate/migrate/v3 v3.5.4 => github.com/golang-migrate/migrate v3.5.4+incompatible
--- go/modules/branch/main.go
+++ go/modules/branch/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/golang-migrate/migrate/v3/database"
+	"github.com/golang-migrate/migrate/driver"
+	database3 "github.com/golang-migrate/migrate/v3/database"
+	database4 "github.com/golang-migrate/migrate/v4/database"
 )
 
 func main() {
-	fmt.Println(database.GenerateAdvisoryLockId("mod test"))
+	fmt.Println(database3.GenerateAdvisoryLockId("mod test"))
+	fmt.Println(database4.GenerateAdvisoryLockId("mod test"))
+	fmt.Println(driver.GetDriver("mod test"))
 }
