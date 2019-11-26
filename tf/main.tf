e92979879901d5d0432d7c8d50a564122ddcbd6c
budougumi0617 <budougumi0617@gmail.com>
2019-11-26 13:47:45 +0900

    :gear: use external resource

--- tf/main.tf
+++ tf/main.tf
@@ -8,8 +8,24 @@ locals {
   example_instance_type = "t3.micro"
 }
 
+// 外部データを参照する。filterで検索条件を指定して最新のAMIを取得しているだけ。
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0f9ae750e8274075b"
+  ami           = data.aws_ami.recent_amazon_linux_2.image_id
   instance_type = var.example_instance_type
   // instance_type = locals.example_instance_type
 
