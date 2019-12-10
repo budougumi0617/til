d910cff20ec074a290a3cb49080b698c1fd90a60
budougumi0617 <budougumi0617@gmail.com>
2019-12-10 13:25:52 +0900

    :sparkles: 6.1 define S3 bucket

---
+++ tf/ecs_project/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "private" {
+  bucket = "budougumi0617-pragmatic-terraform-on-aws"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
