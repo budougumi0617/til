8e67ea2680c06bcb4d331294d975dae3b29dc73a
budougumi0617 <budougumi0617@gmail.com>
2019-12-10 13:22:20 +0900

    :heart: define allow_describe_regions

--- tf/ecs_project/iam_role.tf
+++ tf/ecs_project/iam_role.tf
@@ -4,3 +4,11 @@ module "describe_regions_for_ec2" {
   identifier = "ec2.amazonaws.com"
   policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    resources = ["*"]
+  }
+}
