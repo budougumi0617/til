4bc163535fc63a3d8537030c51a0b772f0259c73
budougumi0617 <budougumi0617@gmail.com>
2019-12-24 14:02:29 +0900

    :sparkles: ch08 8.2.2 add http listener

--- tf/ecs_project/alb.tf
+++ tf/ecs_project/alb.tf
@@ -29,7 +29,9 @@ output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
 
+# ---------------------------------------
 # ALB用のセキュリティグループ
+# ---------------------------------------
 module "http_sg" {
   source      = "./security_group"
   name        = "http-sg"
@@ -52,4 +54,24 @@ module "http_redirect_sg" {
   vpc_id      = aws_vpc.example.id
   port        = 8080
   cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+# ---------------------------------------
+# リスナー
+# ---------------------------------------
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは『HTTP』です"
+      status_code  = "200"
+    }
+  }
 }
