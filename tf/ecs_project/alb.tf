2da54c5e5a36b691ef6cbd32426260feb2273cbf
budougumi0617 <budougumi0617@gmail.com>
2019-12-24 13:55:24 +0900

    :sparkles: ch08 8.2.1 add ALB and security group

---
+++ tf/ecs_project/alb.tf
@@ -0,0 +1,55 @@
+resource "aws_lb" "example" {
+  name                       = "example"
+  # application == ALB, network === NLB
+  load_balancer_type         = "application"
+  # VPC内部向け
+  internal                   = false
+  idle_timeout               = 60
+  # 削除保護。本当はtrueのほうがよい。
+  enable_deletion_protection = false
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id,
+  ]
+
+  access_logs {
+    bucket  = aws_s3_bucket.alb_log.id
+    enabled = true
+  }
+
+  security_groups = [
+    module.http_sg.security_group_id,
+    module.https_sg.security_group_id,
+    module.http_redirect_sg.security_group_id,
+  ]
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+}
+
+# ALB用のセキュリティグループ
+module "http_sg" {
+  source      = "./security_group"
+  name        = "http-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source      = "./security_group"
+  name        = "https-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_redirect_sg" {
+  source      = "./security_group"
+  name        = "http-redirect-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 8080
+  cidr_blocks = ["0.0.0.0/0"]
+}
