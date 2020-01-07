b07f31f34c654e6342775c937b28e9d56f4c8915
budougumi0617 <budougumi0617@gmail.com>
2020-01-07 13:52:33 +0900

    :sparkles: 8.6.2 define SSL setting and listener rule

--- tf/ecs_project/network.tf
+++ tf/ecs_project/network.tf
@@ -166,4 +166,102 @@ resource "aws_route53_record" "example" {
   }
 }
 
+# --------------------------------------
+# SSL settings
+# --------------------------------------
+resource "aws_acm_certificate" "example" {
+  domain_name               = data.aws_route53_zone.budougumi0617.name
+  subject_alternative_names = []
+  validation_method         = "DNS" // 自動更新できる。
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_route53_record" "example_certificate" {
+  name    = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+  zone_id = data.aws_route53_zone.budougumi0617.id
+  ttl     = 60
+}
+
+// apply時にSSL証明書の検証が完了するまで待機する設定。
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.example.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "これは『HTTPS』です"
+      status_code  = "200"
+    }
+  }
+}
+
+// HTTPからHTTPSにするリダイレクトルール
+resource "aws_lb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_target_group" "example" {
+  name                 = "example"
+  vpc_id               = aws_vpc.example.id
+  target_type          = "ip" // ECS FargateではIPアドレスによるルーティングが必要。
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 300
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
+  }
+
+  depends_on = [aws_lb.example] // ECSサービスが完成してからという依存関係
+}
+
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["/*"]
+  }
 }
