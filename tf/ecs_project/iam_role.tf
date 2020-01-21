292558461d9fd4abae4d537e538dab99a93faea8
budougumi0617 <budougumi0617@gmail.com>
2020-01-21 13:29:49 +0900

    :sparkles: P73 10.2.2 add IAM role for batch

--- tf/ecs_project/iam_role.tf
+++ tf/ecs_project/iam_role.tf
@@ -37,3 +37,17 @@ module "ecs_task_execution_role" {
   identifier = "ecs-tasks.amazonaws.com"
   policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
+
+# 第10章バッチ
+
+# 10.4 CloudWatchイベントIAMロールの定義
+module "ecs_events_role" {
+  source     = "./iam_role"
+  name       = "ecs-events"
+  identifier = "events.amazonaws.com"
+  policy     = data.aws_iam_policy.ecs_events_role_policy.policy
+}
+
+data "aws_iam_policy" "ecs_events_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
