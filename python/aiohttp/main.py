1ca366ef6f054a3c89220e7e566434f75ffa294b
budougumi0617 <budougumi0617@gmail.com>
2020-04-08 00:46:34 +0900

    :sparkles: add context sample

---
+++ python/aiohttp/main.py
@@ -0,0 +1,26 @@
+from contextvars import ContextVar
+from aiohttp import web
+from aiohttp.web import middleware
+from aiohttp.web_app import Application
+from aiohttp.web_request import Request
+from aiohttp.web_response import StreamResponse
+
+USER_ID_CONTEXT = ContextVar('VAR', default='default')
+
+
+@middleware
+async def user_id_middleware(request: Request, handler) -> StreamResponse:
+    if request.headers is not None and request.headers.get('x-user-id', '') != '':
+        USER_ID_CONTEXT.set(request.headers.get('x-user-id'))
+    return await handler(request)
+
+
+async def handler(request: Request) -> StreamResponse:
+    user_id: str = USER_ID_CONTEXT.get()
+    return web.Response(text='previous user_id:' + user_id + '\n')
+
+app: Application = web.Application()
+app.middlewares.append(user_id_middleware)
+app.router.add_get('/', handler)
+
+web.run_app(app, port=33000)
