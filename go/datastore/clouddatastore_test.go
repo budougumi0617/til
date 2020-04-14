5cca142197a3ce8d2675880f57d021061c1e4f33
budougumi0617 <budougumi0617@gmail.com>
2020-04-14 23:28:43 +0900

    :white_check_mark: datastore: confirm datastore

---
+++ go/datastore/clouddatastore_test.go
@@ -0,0 +1,37 @@
+package datastore
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"cloud.google.com/go/datastore"
+	"go.mercari.io/datastore/clouddatastore"
+)
+
+type CloudDatastoreStruct struct {
+	Test string
+}
+
+func TestCloudDatastore_Put(t *testing.T) {
+	err := os.Setenv("DATASTORE_EMULATOR_HOST", "localhost:18081")
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := context.Background()
+	cli, _ := datastore.NewClient(ctx, "budougumi0617")
+	client, err := clouddatastore.FromClient(ctx, cli)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer client.Close()
+	// defer cleanUp() need if delete data after test
+
+	key := client.IncompleteKey("CloudDatastoreStruct", nil)
+	key, err = client.Put(ctx, key, &CloudDatastoreStruct{"Hi!"})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	t.Logf("key: %s", key.String())
+}
---
+++ go/datastore/go.mod
@@ -0,0 +1,18 @@
+module github.com/budougumi0617/til/go/datastore
+
+go 1.14
+
+require (
+	cloud.google.com/go/datastore v1.0.0
+	github.com/golang/protobuf v1.4.0 // indirect
+	go.mercari.io/datastore v1.6.1
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
+	golang.org/x/tools v0.0.0-20200414001008-ae52e4b55789 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/genproto v0.0.0-20200413115906-b5235f65be36 // indirect
+	google.golang.org/grpc v1.28.1 // indirect
+	honnef.co/go/tools v0.0.1-2020.1.3 // indirect
+)
