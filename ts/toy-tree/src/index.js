a552707b0720ece75e3be092e50995e53fc616ca
budougumi0617 <budougumi0617@gmail.com>
2020-07-08 00:34:28 +0900

    :sparkles: implement easy tree command

--- ts/toy-tree/src/index.js
+++ ts/toy-tree/src/index.js
@@ -1,24 +1,35 @@
 const meow = require('meow');
+const { read } = require('./read');
+const { format } = require('./format');
 
 exports.main = (argv, stdout, stderr) => {
-    const cli = meow(
-        `
+  const cli = meow(
+    `
+    Usage
+      $ toy-tree <directory>
+    Examples
+      $ toy-tree
+      $ toy-tree path/to/dir
+`,
+    {
+      argv,
+    },
+  );
 
-        Usage
-            $ toy-tree <directory>
-        
-        Examples
-            $ toy-tree
-            $ toy-tree path/to/dir
-        `,
-        {
-            argv,
-        },
-    );
+  const dir = cli.input[0] || '.';
 
-    const dir = cli.input[0] || '.';
+  let root;
 
-    stdout(dir);
-    
-    return 0;
-}
+  try {
+    root = read(dir);
+  } catch (e) {
+    stderr(`Error: ${e.message}`);
+    return 1;
+  }
+
+  const output = format(root);
+
+  stdout(output);
+
+  return 0;
+};
