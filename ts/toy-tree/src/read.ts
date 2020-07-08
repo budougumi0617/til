d17d500dbcbec75a67e870605920f91b5453e089
budougumi0617 <budougumi0617@gmail.com>
2020-07-08 11:46:00 +0900

    :sparkles: add type

--- ts/toy-tree/src/read.ts
+++ ts/toy-tree/src/read.ts
@@ -1,13 +1,13 @@
 import fs from "fs";
 import path from "path";
-import { TODO_any } from "./types";
+import { Options, TreeNode, DirectoryNode } from "./types";
 
-const readDirectory = (dir, depth, options) => {
+const readDirectory = (dir: string, depth: number, options: Options) => {
   if (options.level < depth) {
     return [];
   }
   const dirents = fs.readdirSync(dir, { withFileTypes: true });
-  const nodes: TODO_any = [];
+  const nodes: TreeNode[] = [];
 
   dirents.forEach((dirent) => {
     if (dirent.name.startsWith(".")) {
@@ -35,8 +35,8 @@ const readDirectory = (dir, depth, options) => {
   return nodes;
 };
 
-export const read = (dir, options) => {
-  let stat;
+export const read = (dir: string, options: Options) => {
+  let stat: fs.Stats;
 
   try {
     stat = fs.statSync(dir);
@@ -48,7 +48,7 @@ export const read = (dir, options) => {
     throw new Error(`"${dir}" can't be opened as a directory.`);
   }
 
-  const root = {
+  const root: DirectoryNode = {
     type: "directory",
     name: dir,
     children: readDirectory(dir, 1, options),
--- ts/toy-tree/src/types.ts
+++ ts/toy-tree/src/types.ts
@@ -1,1 +1,18 @@
 export type TODO_any = any;
+
+interface BaseNode<T> {
+  type: T;
+  name: string;
+}
+
+export interface FileNode extends BaseNode<"file"> {}
+
+export interface DirectoryNode extends BaseNode<"directory"> {
+  children: TreeNode[];
+}
+
+export type TreeNode = FileNode | DirectoryNode;
+
+export interface Options {
+  level: number;
+}
