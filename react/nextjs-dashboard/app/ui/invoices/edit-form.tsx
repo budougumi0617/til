003856af28437d1902af47d2687e158a3bc2bbed
budougumi0617 <budougumi0617@gmail.com>
2023-12-10 23:15:03 +0900

    chore(nextjs): update tsconfig

--- react/nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ react/nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
   ClockIcon,
@@ -8,6 +7,8 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import { Button } from '@/app/ui/button';
 
 export default function EditInvoiceForm({
