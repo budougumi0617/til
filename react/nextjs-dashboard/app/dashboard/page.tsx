2bc62fdea6eb4448a130d9668438a907bf98332b
budougumi0617 <budougumi0617@gmail.com>
2023-12-18 22:20:47 +0900

    feat(nextjs): show RevenueChart

--- react/nextjs-dashboard/app/dashboard/page.tsx
+++ react/nextjs-dashboard/app/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueClient from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} text-x1 md:text-2x-1 mb-4`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
