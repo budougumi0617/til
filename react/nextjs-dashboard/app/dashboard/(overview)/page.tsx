61b8649e60551d1e739eb516a040ee7e30d08152
budougumi0617 <budougumi0617@gmail.com>
2024-01-03 15:08:57 +0900

    feat(nextjs): コンポーネント別にSuspenseを適用する

--- react/nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ react/nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -2,16 +2,15 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueClient from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { Suspense } from 'react';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -34,8 +33,12 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
