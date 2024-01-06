88c4d06c7da2948d2c94f357ef59b8e20ca03b4c
budougumi0617 <budougumi0617@gmail.com>
2024-01-06 15:46:24 +0900

    feat(nextjs): debounceを使ったページネーション

--- react/nextjs-dashboard/app/ui/search.tsx
+++ react/nextjs-dashboard/app/ui/search.tsx
@@ -2,21 +2,23 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const serachParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(serachParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
