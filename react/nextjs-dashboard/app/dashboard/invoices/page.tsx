9fc0c7f38ca671f0f480a7f77d5aec59652a0610
budougumi0617 <budougumi0617@gmail.com>
2024-01-06 16:01:16 +0900

    fix(nextjs): fix typo

--- react/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ react/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -8,15 +8,15 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function Page({
-  serachParams,
+  searchParams,
 }: {
-  serachParams?: {
+  searchParams?: {
     query?: string;
     page?: string;
   };
 }) {
-  const query = serachParams?.query || '';
-  const currentPage = Number(serachParams?.page) || 1;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
   const totalPages = await fetchInvoicesPages(query);
 
