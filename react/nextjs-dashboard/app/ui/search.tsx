c8d6d14ed905618e232cd686fea7baea896afe8c
budougumi0617 <budougumi0617@gmail.com>
2024-01-06 15:21:52 +0900

    feat(nextjs): URLに検索パラメータを追加する。デフォルト文字列を検索パラメータから取得する

--- react/nextjs-dashboard/app/ui/search.tsx
+++ react/nextjs-dashboard/app/ui/search.tsx
@@ -1,10 +1,21 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const serachParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(serachParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
@@ -18,6 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={serachParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
