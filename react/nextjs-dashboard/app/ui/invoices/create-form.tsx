d7a0d9602f8984ad0dfb943c5fba623a174b4049
budougumi0617 <budougumi0617@gmail.com>
2024-01-08 10:19:20 +0900

    feat(nextjs): Server Acvtionsとフォームを追加

--- react/nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ react/nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -8,10 +8,11 @@ import Link from 'next/link';
 
 import { CustomerField } from '@/app/lib/definitions';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
