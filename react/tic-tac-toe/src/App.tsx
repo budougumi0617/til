79ae64c8634cf6372bdddfe2387480aaa0f95d5c
budougumi0617 <budougumi0617@gmail.com>
2024-09-29 14:48:18 +0900

    feat(react): use state

--- react/tic-tac-toe/src/App.tsx
+++ react/tic-tac-toe/src/App.tsx
@@ -1,8 +1,10 @@
 import "./index.css";
+import { useState } from "react";
 
-function Square({ value }: { value: string }): JSX.Element {
+function Square(): JSX.Element {
+  const [value, setValue] = useState(null as string | null);
   function handleClick() {
-    console.log("clicked");
+    setValue("X");
   }
   return (
     <button className="square" onClick={handleClick}>
@@ -15,19 +17,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
