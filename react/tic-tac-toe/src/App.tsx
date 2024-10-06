aabfdc26c26a63892df1007a5f2087414a116302
budougumi0617 <budougumi0617@gmail.com>
2024-10-06 11:10:37 +0900

    feat(raect): lift up states

--- react/tic-tac-toe/src/App.tsx
+++ react/tic-tac-toe/src/App.tsx
@@ -1,35 +1,43 @@
 import "./index.css";
 import { useState } from "react";
 
-function Square(): JSX.Element {
-  const [value, setValue] = useState(null as string | null);
-  function handleClick() {
-    setValue("X");
-  }
+function Square({
+  value,
+  onSquareClick,
+}: {
+  value: string | null;
+  onSquareClick: () => void;
+}): JSX.Element {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array<string | null>(9).fill(null));
+  const handleClick = () => {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  };
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </>
   );
