7580b0319992fab8654a0aac96e1e842db793ac6
budougumi0617 <budougumi0617@gmail.com>
2024-10-06 11:42:57 +0900

    feat(react): record history

--- react/tic-tac-toe/src/App.tsx
+++ react/tic-tac-toe/src/App.tsx
@@ -15,11 +15,15 @@ function Square({
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
-  const [squares, setSquares] = useState<Array<string | null>>(
-    Array<string | null>(9).fill(null),
-  );
+export function Board({
+  xIsNext,
+  squares,
+  onPlay,
+}: {
+  xIsNext: boolean;
+  squares: Array<string | null>;
+  onPlay: (nextSquares: Array<string | null>) => void;
+}): JSX.Element {
   const winner = calculateWinner(squares);
   let status: string;
   if (winner) {
@@ -40,8 +44,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   };
   return (
     <>
@@ -62,6 +65,28 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [history, setHistory] = useState<Array<Array<string | null>>>([
+    Array(9).fill(null),
+  ]);
+  const currentSquares = history[history.length - 1];
+  function handlePlay(nextSquares: Array<string | null>): void {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
