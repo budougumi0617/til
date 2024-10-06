3ae8dd8a9a02b96bc470b7e50412215ce7283de8
budougumi0617 <budougumi0617@gmail.com>
2024-10-06 11:51:35 +0900

    feat(react): implement history

--- react/tic-tac-toe/src/App.tsx
+++ react/tic-tac-toe/src/App.tsx
@@ -73,18 +73,40 @@ export default function Game() {
   const [history, setHistory] = useState<Array<Array<string | null>>>([
     Array(9).fill(null),
   ]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState<number>(0);
+  const currentSquares = history[currentMove];
   function handlePlay(nextSquares: Array<string | null>): void {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
+  function jumpTo(nextMove: number): void {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares: Array<string | null>, move: number) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
